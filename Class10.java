6c9fdfe1a8ebbdf7a50bdbc9ce304c324d9754b9
Kuan9302 <129488729+Kuan9302@users.noreply.github.com>
2023-10-12 21:49:07 +0800

    Class10.java

---
+++ Class10.java
@@ -0,0 +1,25 @@
+// (a)
+class CRational {
+ public  int n;
+ public  int d;
+ public  void setND(int num, int den) {
+  n=num;
+  d=den;
+ }
+ public void setN(int num) {
+ n=num;
+ }
+ public void setD(int num) {
+ d=num;
+ }
+ public void show() {
+ System.out.println(n+"/"+d);
+ }
+}
+ public class eel {
+ public static void main(String args[]) {
+ CRational aaa=new CRational(); 
+ aaa.setND(3,5);
+ aaa.show();
+ }
+}
