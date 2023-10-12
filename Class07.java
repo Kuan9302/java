e5cd58031b9b5276328835b6094b5492b27bdfa4
Kuan9302 <129488729+Kuan9302@users.noreply.github.com>
2023-10-12 21:39:52 +0800

    Class07.java

---
+++ Class07.java
@@ -0,0 +1,15 @@
+class Count {
+ public static void ppp(int i,int j) {
+  System.out.println(Math.pow(i, j));   
+ }
+}
+public class eel {
+ public static void main(String args[]) {
+  Count.ppp(2, 5);
+  Count.ppp(3, 2);
+ }
+}
+
+//印出
+32.0
+9.0
